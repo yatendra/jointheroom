f083824b8ac020b71491ad50d21cad9d4fc250cf
Yatendra Khandelwal <yatendra.khandelwal@ishisystems.com>
2014-02-20 16:08:39 -0500

    fixes

--- js/appObject.js
+++ js/appObject.js
@@ -65,6 +65,7 @@
             } else {
                 alert("stopping");
                 appObject.webrtc.stopLocalVideo();
+                ($("#localVideo").removeAttr("src");
                 setVideoButton(true);
             }
         });
